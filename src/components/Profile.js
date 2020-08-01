84b9c2e4f97046cc9bb383df048fdca6b93ecfb4
priyam1103 <priyampoddar89@gmail.com>
2020-08-02 00:24:57 +0530

    added loading state in profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { navigate } from "@reach/router";
 import i1 from "../assets/image1.jpg";
 import i3 from "../assets/i3.jpg";
@@ -12,12 +12,12 @@ const Profile = () => {
   const [loading, setLoading] = useState(false);
   const user = useSelector((state) => state.user.user);
   useEffect(() => {
-     setLoading(true);
+    setLoading(true);
     const token = localStorage.getItem("tokenn");
     if (!token) {
       navigate("/");
     }
-     setLoading(false);
+    setLoading(false);
   }, []);
   const handleLogout = () => {
     navigate("/");
@@ -26,7 +26,7 @@ const Profile = () => {
 
   return (
     <div>
-     {loading ? (
+      {loading ? (
         <Loader
           style={{
             marginTop: "10%",
@@ -37,77 +37,84 @@ const Profile = () => {
           width={100}
         />
       ) : (
-    <>
-      {user ? (
         <>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              marginLeft: "5%",
-              justifyContent: "space-between",
-            }}
-          >
-            <p>
-              Hi,<span style={{ color: "red" }}> {user.username}</span> your
-              type is <span style={{ color: "red" }}>{user.userType}</span>
-            </p>
-            <button style={{ padding: 10, margin: 10 }} onClick={handleLogout}>
-              Logout
-            </button>
-          </div>
-          <div style={{}}>
-            {user.userType === "A" ? (
-              <img
-                style={{ width: window.innerWidth, height: window.innerHeight }}
-                src={i1}
-                alt="i2"
-              ></img>
-            ) : null}
-            {user.userType === "B" ? (
-              <div style={{ display: "flex" }}>
-                <img
-                  style={{
-                    height: window.innerHeight,
-                    width: window.innerWidth / 2,
-                  }}
-                  alt="i1"
-                  src={i1}
-                ></img>
-                <img
-                  style={{
-                    height: window.innerHeight,
-                    width: window.innerWidth / 2,
-                  }}
-                  src={i2}
-                  alt="i2"
-                ></img>
+          {user ? (
+            <>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  marginLeft: "5%",
+                  justifyContent: "space-between",
+                }}
+              >
+                <p>
+                  Hi,<span style={{ color: "red" }}> {user.username}</span> your
+                  type is <span style={{ color: "red" }}>{user.userType}</span>
+                </p>
+                <button
+                  style={{ padding: 10, margin: 10 }}
+                  onClick={handleLogout}
+                >
+                  Logout
+                </button>
               </div>
-            ) : null}
-            {user.userType === "C" ? (
-              <div style={{ display: "flex", flexDirection: "column" }}>
-                <img
-                  style={{
-                    height: window.innerHeight / 2,
-                    width: window.innerWidth,
-                  }}
-                  src={i2}
-                  alt="i1"
-                ></img>
-                <img
-                  style={{
-                    height: window.innerHeight / 2,
-                    width: window.innerWidth,
-                  }}
-                  src={i3}
-                  alt="i2"
-                ></img>
+              <div style={{}}>
+                {user.userType === "A" ? (
+                  <img
+                    style={{
+                      width: window.innerWidth,
+                      height: window.innerHeight,
+                    }}
+                    src={i1}
+                    alt="i2"
+                  ></img>
+                ) : null}
+                {user.userType === "B" ? (
+                  <div style={{ display: "flex" }}>
+                    <img
+                      style={{
+                        height: window.innerHeight,
+                        width: window.innerWidth / 2,
+                      }}
+                      alt="i1"
+                      src={i1}
+                    ></img>
+                    <img
+                      style={{
+                        height: window.innerHeight,
+                        width: window.innerWidth / 2,
+                      }}
+                      src={i2}
+                      alt="i2"
+                    ></img>
+                  </div>
+                ) : null}
+                {user.userType === "C" ? (
+                  <div style={{ display: "flex", flexDirection: "column" }}>
+                    <img
+                      style={{
+                        height: window.innerHeight / 2,
+                        width: window.innerWidth,
+                      }}
+                      src={i2}
+                      alt="i1"
+                    ></img>
+                    <img
+                      style={{
+                        height: window.innerHeight / 2,
+                        width: window.innerWidth,
+                      }}
+                      src={i3}
+                      alt="i2"
+                    ></img>
+                  </div>
+                ) : null}
               </div>
-            ) : null}
-          </div>
+            </>
+          ) : null}
         </>
-      ) : null}
-      <>)}
+      )}
     </div>
   );
 };
